c019003b4e01f3e30015551c627d6cf744b85f99
B4Le <48969870+B4Lee@users.noreply.github.com>
2019-11-03 23:21:38 +0700

    Update form-process.php

--- php/form-process.php
+++ php/form-process.php
@@ -32,8 +32,8 @@ if (empty($_POST["message"])) {
 }
 
 
-$EmailTo = "armanmia7@gmail.com";
-$Subject = "New Message Received";
+$EmailTo = "airitservice19@gmail.com";
+$Subject = "AIR IT SERVICE";
 
 // prepare email body text
 $Body = "";
